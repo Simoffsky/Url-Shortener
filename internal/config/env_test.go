c51811afc096ed0cc741914030e3c3722e5c6c2b
Simoffsky <maks-kirsanov26@mail.ru>
2024-06-17 11:56:18 +1000

    changed env config tests

--- internal/config/env_test.go
+++ internal/config/env_test.go
@@ -11,10 +11,11 @@ import (
 func TestNewEnvConfig(t *testing.T) {
 	os.Setenv("PORT", "5000")
 	os.Setenv("HTTP_TIMEOUT", "15")
-
+	os.Setenv("LOGGER_LEVEL", "DEBUG")
 	expected := Config{
 		Port:        "5000",
 		HTTPTimeout: 15 * time.Second,
+		LoggerLevel: "DEBUG",
 	}
 
 	config := NewEnvConfig()
@@ -23,15 +24,17 @@ func TestNewEnvConfig(t *testing.T) {
 
 	os.Unsetenv("PORT")
 	os.Unsetenv("HTTP_TIMEOUT")
+	os.Unsetenv("LOGGER_LEVEL")
 }
 
 func TestNewEnvConfigWithDefaults(t *testing.T) {
 	os.Unsetenv("PORT")
 	os.Unsetenv("HTTP_TIMEOUT")
-
+	os.Unsetenv("LOGGER_LEVEL")
 	expected := Config{
 		Port:        "8080",
 		HTTPTimeout: 10 * time.Second,
+		LoggerLevel: "DEBUG",
 	}
 
 	config := NewEnvConfig()
